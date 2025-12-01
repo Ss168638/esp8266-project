b3dd93fee304d4c0299289479c2bf39b2d2707ec
Suraj Sharma <ss168638@gmail.com>
2025-12-01 22:48:11 +0530

    fuck you

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <lib/OTAUpdater/OTAUpdater.h>
+#include <OTAUpdater.h>
 const char* ssid="SURAJ";const char* pass="ss168638@";
 const char* VERSION_URL="https://raw.githubusercontent.com/Ss168638/esp8266-project/main/firmware/version.json";
 const char* FIRMWARE_URL="https://raw.githubusercontent.com/Ss168638/esp8266-project/main/firmware/firmware.bin?raw=1";
